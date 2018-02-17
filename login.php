37bc880a9c8a4bb9f3876ea417856bb6b96472ab
haowei99 <35088641+haowei99@users.noreply.github.com>
2018-02-17 18:32:50 -0500

    Add files via upload

---
+++ login.php
@@ -0,0 +1,37 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registration system PHP and MySQL</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body style="background-image: url(https://wallpapers.walldevil.com/wallpapers/a45/preview/night-day-silhouette-moon-sun-sky-tree-owl-boy.jpg)">
+
+	<div class="header" style="background-color: #9b9999">
+		<h2>Login</h2>
+	</div>
+
+	<form method="post" action="login.php">
+
+		<?php include('errors.php'); ?>
+
+		<div class="input-group">
+			<label>Username</label>
+			<input type="text" name="username" >
+		</div>
+		<div class="input-group">
+			<label>Password</label>
+			<input type="password" name="password">
+		</div>
+		<div class="input-group">
+			<button type="submit" class="btn" name="login_user"
+			style="background-color: #9b9999">Login</button>
+		</div>
+		<p>
+			Not yet a member? <a href="register.php">Sign up</a>
+		</p>
+	</form>
+
+
+</body>
+</html>
